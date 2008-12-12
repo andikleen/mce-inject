93cf332ea98d9ab4847e8129c63e7ff1ccc37ea0
Andi Kleen <ak@linux.intel.com>
2008-12-12 14:06:55 +0100

    Add xcalloc()
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- util.c
+++ util.c
@@ -2,13 +2,25 @@
 #include <stdio.h>
 #include "util.h"
 
+void oom(void)
+{
+	fprintf(stderr, "Out of virtual memory\n");
+	exit(1);
+}
+
+void *xcalloc(size_t a, size_t b)
+{
+	void *p = calloc(a, b);
+	if (!p)
+		oom();
+	return p;
+}
+
 void *xalloc(size_t sz)
 {
 	void *p = calloc(sz, 1);
-	if (!p) {
-		fprintf(stderr, "Out of virtual memory\n");
-		exit(1);
-	}
+	if (!p)
+		oom();
 	return p;
 }
 
--- util.h
+++ util.h
@@ -3,6 +3,7 @@ void err(const char *msg);
 
 #define NEW(x) ((x) = xalloc(sizeof(*(x))))
 void *xalloc(size_t sz);
+void *xcalloc(size_t a, size_t b);
 
 #ifdef __GNUC__
 #define barrier() asm volatile("" ::: "memory")
